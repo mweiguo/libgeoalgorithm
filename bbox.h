9becd75c103fcc735a55ecd30c4d12d189e9ba88
mweiguo <mweiguo@gmail.com>
2010-03-22 10:21:23 +0000

    修改了bbox初始化接口，添加了dump功能

--- bbox.h
+++ bbox.h
@@ -3,18 +3,22 @@
 
 #include "vec3.h"
 #include "algorithm"
+#include <tinylog.h>
 
 class BBox
 {
 public:
-  typedef vec3<float> vec3f;
+  // constructor
   BBox () {}
   BBox ( const BBox& rhs ) { _min=rhs.min(); _max=rhs.max(); }
   BBox ( vec3f& min, vec3f& max ) : _min(min), _max(max) {}
+  // min/max
   const vec3f& min() const { return _min; }
   const vec3f& max() const { return _max; }
-  void min ( vec3f& v ) { _min = v; }
-  void max ( vec3f& v ) { _max = v; }
+  vec3f& min() { return _min; }
+  vec3f& max() { return _max; }
+  void init ( const vec3f& v ) { _min = v; _max=v; }
+  // other operations
   vec3f center () { return ( _min + _max ) / 2; }
   void expandby ( const vec3f& pnt ) {
     _min = _min.min ( pnt );
@@ -25,6 +29,11 @@ public:
     t.expandby ( rhs.min() );
     t.expandby ( rhs.max() );
     return t;
+  }
+  void dump () {
+#ifdef _DEBUG_OUTPUT_
+    LOG_DEBUG ( "min=(%f,%f,%f)\tmax=(%f,%f,%f)", _min.x(), _min.y(), _min.z(), _max.x(), _max.y(), _max.z() );
+#endif
   }
 private:
   vec3f _min, _max;
